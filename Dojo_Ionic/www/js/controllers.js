ea8552f0cf05b165abf368f31d0a23e0c977c7c8
Alexis Rodríguez Gtz <alexisrodgtz@gmail.com>
2015-11-07 09:18:19 -0500

    Creación proyecto, creación controlador 'controller', integración de controle en app.js y modificación de index.html para mostra la información obtenida de Reddit por medio del controller

---
+++ Dojo_Ionic/www/js/controllers.js
@@ -0,0 +1,17 @@
+angular.module('app.controllers',[])
+
+.controller('RedditCtrl', ['$scope','$http', function($scope, $http){
+
+  $scope.posts = [];
+  
+  $http.get('https://www.reddit.com/r/gaming/.json').success(function(posts){
+        
+        angular.forEach(posts.data.children, function(post){
+    $scope.posts.push(post.data);
+            
+        });
+
+
+
+    });
+}
