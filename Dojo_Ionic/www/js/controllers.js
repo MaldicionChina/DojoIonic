f29d0d9a2673f5cc7524197661a63c4780525a1d
Alexis Rodríguez Gtz <alexisrodgtz@gmail.com>
2015-11-07 09:55:34 -0500

    Instalaión de angular-moment para mostrar el tiempo en que fueron publicados los post

--- Dojo_Ionic/www/js/controllers.js
+++ Dojo_Ionic/www/js/controllers.js
@@ -1,17 +1,19 @@
-angular.module('app.controllers',[])
+angular.module('starter.controllers',[])
 
 .controller('RedditCtrl', ['$scope','$http', function($scope, $http){
 
-  $scope.posts = [];
-  
-  $http.get('https://www.reddit.com/r/gaming/.json').success(function(posts){
+    $scope.posts = [];
+
+    $http.get('https://www.reddit.com/r/gaming/new/.json').success(function(posts){
         
         angular.forEach(posts.data.children, function(post){
-    $scope.posts.push(post.data);
+
+            $scope.posts.push(post.data);
             
         });
 
-
-
-    });
-}
+    }); 
+     
+        
+}]);
+    
